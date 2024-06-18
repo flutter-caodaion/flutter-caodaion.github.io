a5989165fb3c65a2785c994ad1baad0a3cf881db
Nguyễn Trung Nhân <52454131+nhanntdl@users.noreply.github.com>
2024-06-18 12:40:47 +0700

    adl

--- lib/pages/library/library_page.dart
+++ lib/pages/library/library_page.dart
@@ -75,123 +75,130 @@ class _LibraryPageState extends State<LibraryPage> {
             ),
           ),
           SliverToBoxAdapter(
-            child: _displayBooks.isNotEmpty ? Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Wrap(
-                alignment: WrapAlignment.center,
-                spacing: 8.0,
-                children: _displayBooks.map((book) {
-                  double screenWidth = MediaQuery.of(context).size.width;
-                  double cardWidth = (screenWidth /
-                          (ResponsiveBreakpoints.of(context).isMobile
-                              ? 1
-                              : ResponsiveBreakpoints.of(context).isTablet
-                                  ? 2
-                                  : ResponsiveBreakpoints.of(context).isDesktop
-                                      ? 5
-                                      : 1)) -
-                      8 -
-                      2;
-                  List labels = jsonDecode(book['labels'] ?? '[]');
-                  return SizedBox(
-                    width: cardWidth,
-                    child: Card(
-                      color: ColorConstants.whiteBackdround,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          ListTile(
-                            leading: book['finished'] == 'true'
-                                ? const Icon(
-                                    Icons.check_circle,
-                                    color: Color(0xff01855d),
-                                  )
-                                : const Icon(
-                                    Icons.history_edu,
-                                    color: Color(0xff4758b8),
+            child: _displayBooks.isNotEmpty
+                ? Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Wrap(
+                      alignment: WrapAlignment.center,
+                      spacing: 8.0,
+                      children: _displayBooks.map((book) {
+                        double screenWidth = MediaQuery.of(context).size.width;
+                        double cardWidth = (screenWidth /
+                                (ResponsiveBreakpoints.of(context).isMobile
+                                    ? 1
+                                    : ResponsiveBreakpoints.of(context).isTablet
+                                        ? 2
+                                        : ResponsiveBreakpoints.of(context)
+                                                .isDesktop
+                                            ? 5
+                                            : 1)) -
+                            8 -
+                            2;
+                        List labels = jsonDecode(book['labels'] ?? '[]');
+                        return SizedBox(
+                          width: cardWidth,
+                          child: Card(
+                            color: ColorConstants.whiteBackdround,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.stretch,
+                              children: [
+                                ListTile(
+                                  leading: book['finished'] == 'true'
+                                      ? const Icon(
+                                          Icons.check_circle,
+                                          color: Color(0xff01855d),
+                                        )
+                                      : const Icon(
+                                          Icons.history_edu,
+                                          color: Color(0xff4758b8),
+                                        ),
+                                  title: Text(
+                                    book['name'],
+                                    overflow: TextOverflow.ellipsis,
+                                    maxLines: 2,
                                   ),
-                            title: Text(
-                              book['name'],
-                              overflow: TextOverflow.ellipsis,
-                              maxLines: 2,
-                            ),
-                          ),
-                          AspectRatio(
-                            aspectRatio: 16 / 10,
-                            child: book['cover'] != ''
-                                ? Image.network(
-                                    book['cover'],
-                                    fit: BoxFit.cover,
-                                  )
-                                : SvgPicture.asset(
-                                    'assets/icons/book-bookmark-minimalistic-svgrepo-com.svg',
-                                    fit: BoxFit.contain,
-                                  ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(16.0),
-                            child: Text(
-                              book['description'],
-                              textAlign: TextAlign.start,
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Wrap(
-                              spacing: 8.0,
-                              children: labels.map((label) {
-                                return ElevatedButton(
-                                  style: ElevatedButton.styleFrom(
-                                    backgroundColor: const Color(0xffe0e0e0),
-                                  ),
-                                  onPressed: () {},
+                                ),
+                                AspectRatio(
+                                  aspectRatio: 16 / 10,
+                                  child: book['cover'] != ''
+                                      ? Image.network(
+                                          book['cover'],
+                                          fit: BoxFit.cover,
+                                        )
+                                      : SvgPicture.asset(
+                                          'assets/icons/book-bookmark-minimalistic-svgrepo-com.svg',
+                                          fit: BoxFit.contain,
+                                        ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(16.0),
                                   child: Text(
-                                    label,
-                                    style: const TextStyle(
-                                      color: Colors.black,
+                                    book['description'],
+                                    textAlign: TextAlign.start,
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Wrap(
+                                    spacing: 8.0,
+                                    children: labels.map((label) {
+                                      return ElevatedButton(
+                                        style: ElevatedButton.styleFrom(
+                                          backgroundColor:
+                                              const Color(0xffe0e0e0),
+                                        ),
+                                        onPressed: () {},
+                                        child: Text(
+                                          label,
+                                          style: const TextStyle(
+                                            color: Colors.black,
+                                          ),
+                                        ),
+                                      );
+                                    }).toList(),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: TextButton(
+                                    onPressed: () {
+                                      context.go("/sach/${book['key']}");
+                                    },
+                                    child: const Wrap(
+                                      crossAxisAlignment:
+                                          WrapCrossAlignment.center,
+                                      alignment: WrapAlignment.center,
+                                      children: [
+                                        Icon(
+                                          Icons.visibility,
+                                          color: Colors.black,
+                                        ),
+                                        SizedBox(
+                                          width: 8,
+                                        ),
+                                        SizedBox(
+                                          width: 8,
+                                        ),
+                                        Text(
+                                          "ĐỌC SÁCH",
+                                          style: TextStyle(
+                                            color: Colors.black,
+                                          ),
+                                        ),
+                                      ],
                                     ),
                                   ),
-                                );
-                              }).toList(),
+                                ),
+                              ],
                             ),
                           ),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: TextButton(
-                              onPressed: () {
-                                context.go("/sach/${book['key']}");
-                              },
-                              child: const Wrap(
-                                crossAxisAlignment: WrapCrossAlignment.center,
-                                alignment: WrapAlignment.center,
-                                children: [
-                                  Icon(
-                                    Icons.visibility,
-                                    color: Colors.black,
-                                  ),
-                                  SizedBox(
-                                    width: 8,
-                                  ),
-                                  SizedBox(
-                                    width: 8,
-                                  ),
-                                  Text(
-                                    "ĐỌC SÁCH",
-                                    style: TextStyle(
-                                      color: Colors.black,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
+                        );
+                      }).toList(),
                     ),
-                  );
-                }).toList(),
-              ),
-            ) : LinearProgressIndicator(),
+                  )
+                : const Center(
+                    child: LinearProgressIndicator(),
+                  ),
           ),
           const SliverToBoxAdapter(
             child: SizedBox(
