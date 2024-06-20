cd6d71ac7a450f74870c805aab097e63f7f1583b
Nguyễn Trung Nhân <52454131+nhanntdl@users.noreply.github.com>
2024-06-20 10:25:04 +0700

    add marker cluster

--- lib/pages/maps/maps_page.dart
+++ lib/pages/maps/maps_page.dart
@@ -2,6 +2,7 @@
 import 'dart:convert';
 import 'package:caodaion/constants/constants.dart';
 import 'package:caodaion/pages/maps/service/maps_service.dart';
+import 'package:flutter_map_marker_cluster/flutter_map_marker_cluster.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
@@ -338,8 +339,32 @@ class _MapsPageState extends State<MapsPage> {
                             ),
                             alignment: Alignment.topCenter,
                           ),
-                          ..._displayMarkers
                         ],
+                      ),
+                      MarkerClusterLayerWidget(
+                        options: MarkerClusterLayerOptions(
+                          maxClusterRadius: 45,
+                          size: const Size(40, 40),
+                          alignment: Alignment.center,
+                          padding: const EdgeInsets.all(50),
+                          maxZoom: 15,
+                          markers: [
+                            ..._displayMarkers
+                          ],
+                          builder: (context, markers) {
+                            return Container(
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(20),
+                                  color: Colors.blue),
+                              child: Center(
+                                child: Text(
+                                  markers.length.toString(),
+                                  style: const TextStyle(color: Colors.white),
+                                ),
+                              ),
+                            );
+                          },
+                        ),
                       ),
                       PolylineLayer(
                         polylines: [
