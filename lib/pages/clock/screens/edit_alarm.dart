a61f5bb4628c7d53c74aaf0dbd490033d80642e2
Nguyễn Trung Nhân <52454131+nhanntdl@users.noreply.github.com>
2024-06-24 15:45:21 +0700

    Revert "add tts v"
    This reverts commit a69bc301fb721c4a4a060898b2b1eb4e328bd1d2.

--- lib/pages/clock/screens/edit_alarm.dart
+++ lib/pages/clock/screens/edit_alarm.dart
@@ -35,8 +35,6 @@ class _ExampleAlarmEditScreenState extends State<ExampleAlarmEditScreen> {
 
   late List<bool> selectedDays = List.filled(7, false);
   late bool active = true;
-  late bool tts1 = true;
-  late bool tts2 = true;
   var alarmSettings;
   var loopData;
 
@@ -47,8 +45,6 @@ class _ExampleAlarmEditScreenState extends State<ExampleAlarmEditScreen> {
 
     selectedDays = List.filled(7, false);
     active = true;
-    tts1 = false;
-    tts2 = false;
     if (creating) {
       selectedDateTime = DateTime.now().add(const Duration(minutes: 1));
       selectedDateTime = selectedDateTime.copyWith(second: 0, millisecond: 0);
@@ -73,8 +69,6 @@ class _ExampleAlarmEditScreenState extends State<ExampleAlarmEditScreen> {
           'fadeDuration': widget.alarmSettings.fadeDuration,
           'selectedDays': List.filled(7, false),
           'active': true,
-          'tts1': tts1,
-          'tts2': tts2,
         };
         alarmSettings = widgetAlarmSettings;
         loopData = widgetAlarmSettings;
@@ -228,8 +222,6 @@ class _ExampleAlarmEditScreenState extends State<ExampleAlarmEditScreen> {
         'fadeDuration': buildedAlarmSettings.fadeDuration,
         'selectedDays': selectedDays,
         'active': true,
-        'tts1': tts1,
-        'tts2': tts2,
       };
       final prefs = await SharedPreferences.getInstance();
       final List loopAlarms = jsonDecode(prefs.getString('loopAlarms') ?? '[]');
@@ -385,40 +377,6 @@ class _ExampleAlarmEditScreenState extends State<ExampleAlarmEditScreen> {
                 Switch(
                   value: vibrate,
                   onChanged: (value) => setState(() => vibrate = value),
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'Đọc tiêu đề hẹn giờ',
-                  style: Theme.of(context).textTheme.titleMedium,
-                ),
-                Switch(
-                  value: tts1,
-                  onChanged: (value) => {
-                    setState(() {
-                      tts1 = value;
-                    })
-                  },
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'Đọc nội dung hẹn giờ',
-                  style: Theme.of(context).textTheme.titleMedium,
-                ),
-                Switch(
-                  value: tts2,
-                  onChanged: (value) => {
-                    setState(() {
-                      tts2 = value;
-                    })
-                  },
                 ),
               ],
             ),
